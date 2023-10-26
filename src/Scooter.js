797e1e4685a2f4c08dac32dfc3ba8719080d6fae
ljenchik <pelagenkolena@gmail.com>
2023-10-26 10:10:30 +0100

    Added Scooter class and tests

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,21 @@
-class Scooter{
-  // scooter code here
+const User = require("./User");
+class Scooter {
+    static nextSertial = 1;
+    constructor(station) {
+        this.station = station;
+        this.user = null;
+        Scooter.nextSertial += 1;
+        this.serial = Scooter.nextSertial;
+        this.charge = 100;
+        this.isBroken = false;
+    }
+    rent(user) {
+        if (this.charge > 20 && !this.isBroken) {
+            return "Ready to check out";
+        } else {
+            throw new Error("Scooter needs to charge or scooter needs repair");
+        }
+    }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,61 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter();
-    expect(scooter).toBeInstanceOf(Scooter);
-  });
-})
+describe("scooter object", () => {
+    const scooter = new Scooter("Limehouse");
+    const scooter1 = new Scooter("Bank");
+    test("Scooter class should create Scooter instance", () => {
+        expect(scooter).toBeInstanceOf(Scooter);
+    });
+    test("correct station", () => {
+        expect(scooter.station).toBe("Limehouse");
+    });
+    test("correct user", () => {
+        expect(scooter.user).toBe(null);
+    });
+    test("correct serial", () => {
+        expect(scooter.serial).toBe(2);
+        expect(scooter1.serial).toBe(3);
+    });
+    test("correct charge to 100", () => {
+        expect(scooter.charge).toBe(100);
+    });
+    test("correct isBroken to false when scooter is not broken", () => {
+        expect(scooter.isBroken).toBe(false);
+    });
+});
 
 //Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe("scooter methods", () => {
+    const scooter = new Scooter("Limehouse");
+    const scooter1 = new Scooter("Bank");
+    const scooter2 = new Scooter("TowerBridge");
+    const user = new User();
+    //rent method
+    test("rent method to checkout to user", () => {
+        expect(scooter.rent(user)).toBe("Ready to check out");
+    });
+    test("rent method to throw error when charge is less than 20", () => {
+        scooter1.charge = 15;
+        expect(() => scooter1.rent(user)).toThrow(
+            "Scooter needs to charge or scooter needs repair"
+        );
+    });
+    test("rent method to throw error when scooter is broken", () => {
+        scooter2.isBroken = true;
+        expect(() => scooter2.rent(user)).toThrow(
+            "Scooter needs to charge or scooter needs repair"
+        );
+    });
+    test("rent method to throw error when charge is low and scooter is broken", () => {
+        scooter2.charge = 19;
+        expect(() => scooter2.rent(user)).toThrow(
+            "Scooter needs to charge or scooter needs repair"
+        );
+    });
 
-  //rent method
-  
-
-  //dock method
-
-  //requestRepair method
-
-  //charge method
-
-})
+    //dock method
+    //requestRepair method
+    //charge method
+});
