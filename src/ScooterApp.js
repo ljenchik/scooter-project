72bac5cfe491690923b4fad3e62d6e72d919b550
ljenchik <pelagenkolena@gmail.com>
2023-10-26 15:07:42 +0100

    Added ScooterApp class and tests for login and logout

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,43 @@
-const User = require('./User')
-const Scooter = require('./Scooter')
+const User = require("./User");
+const Scooter = require("./Scooter");
 
 class ScooterApp {
-  // ScooterApp code here
+    static stations = { station1: [], station2: [], station3: [] };
+    constructor() {
+        this.registeredUsers = {};
+    }
+    registerUser(username, password, age) {
+        if (!this.registeredUsers.hasOwnProperty(username) && age >= 18) {
+            const newRegisteredUser = new User(username, password, age);
+            this.registeredUsers[username] = newRegisteredUser;
+            console.log("User has been registered");
+            return newRegisteredUser;
+        } else if (this.registeredUsers.hasOwnProperty(username)) {
+            throw new Error("Already registered");
+        } else if (age < 18) {
+            throw new Error("Too young to register");
+        }
+    }
+    loginUser(username, password) {
+        if (this.registeredUsers.hasOwnProperty(username)) {
+            const registeredUser = this.registeredUsers[username];
+            registeredUser.login(password);
+            console.log("User has been logged in");
+        } else if (
+            !this.registeredUsers.hasOwnProperty(username) ||
+            !registeredUsers[username].loggedIn
+        ) {
+            throw new Error("Username or password is incorrect");
+        }
+    }
+    logoutUser(username) {
+        if (this.registeredUsers.hasOwnProperty(username)) {
+            const registeredUser = this.registeredUsers[username];
+            registeredUser.logout();
+        } else {
+            throw new Error("No such user is logged in");
+        }
+    }
 }
 
-module.exports = ScooterApp
+module.exports = ScooterApp;
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,22 +1,97 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
-const ScooterApp = require('../src/ScooterApp')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
+const ScooterApp = require("../src/ScooterApp");
 
-const scooterApp = new ScooterApp();
+let scooterApp;
+
+beforeEach(() => {
+    scooterApp = new ScooterApp();
+});
+
 // ScooterApp tests here
+describe("ScooterApp properties tests", () => {
+    test("correct stations", () => {
+        expect(ScooterApp.stations).toEqual({
+            station1: [],
+            station2: [],
+            station3: [],
+        });
+    });
+});
 
 // register user
 describe("registerUser method tests", () => {
-  test("Should return instance of User", () => {
-    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
-    expect(response).toBeInstanceOf(User);
-  });
+    test("Should return instance of User", () => {
+        let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+        expect(response).toBeInstanceOf(User);
+    });
+
+    test("Should add one user to registered users object", () => {
+        scooterApp.registerUser("Jane", "test123", 34);
+        expect(scooterApp.registeredUsers).toEqual({
+            Jane: new User("Jane", "test123", 34),
+        });
+    });
+    test("Should add users to registered users object", () => {
+        scooterApp.registerUser("Jane", "test123", 34);
+        scooterApp.registerUser("Kate", "test123", 23);
+        expect(scooterApp.registeredUsers).toEqual({
+            Jane: new User("Jane", "test123", 34),
+            Kate: new User("Kate", "test123", 23),
+        });
+    });
+
+    test("Should throw error when user already registered", () => {
+        scooterApp.registeredUsers = { Jane: ["test123", 34] };
+        expect(() => scooterApp.registerUser("Jane", "test123", 34)).toThrow(
+            "Already registered"
+        );
+    });
+    test("Should throw error when user is less 18 years old", () => {
+        expect(() => scooterApp.registerUser("Jane", "test123", 17)).toThrow(
+            "Too young to register"
+        );
+    });
+    // log in
+    test("Should login and user.loggedIn = true", () => {
+        scooterApp.registerUser("Jane", "test123", 34);
+        scooterApp.registerUser("Kate", "4321", 23);
+        expect(scooterApp.registeredUsers).toEqual({
+            Jane: { age: 34, loggedIn: false, username: "Jane" },
+            Kate: { age: 23, loggedIn: false, username: "Kate" },
+        });
+        expect(scooterApp.registeredUsers["Jane"].loggedIn).toBe(false);
+        expect(scooterApp.registeredUsers["Kate"].loggedIn).toBe(false);
+        scooterApp.loginUser("Jane", "test123");
+        expect(scooterApp.registeredUsers["Jane"].loggedIn).toBe(true);
+    });
+    // log out
+    test("Should logout correctly", () => {
+        scooterApp.registerUser("Jane", "test123", 34);
+        scooterApp.registerUser("Kate", "4321", 23);
+        scooterApp.loginUser("Jane", "test123");
+        scooterApp.loginUser("Kate", "4321");
+        scooterApp.logoutUser("Jane");
+        expect(scooterApp.registeredUsers).toEqual({
+            Jane: { age: 34, loggedIn: false, username: "Jane" },
+            Kate: { age: 23, loggedIn: true, username: "Kate" },
+        });
+    });
+
+    test("Should logout correctly id all users logged out", () => {
+        scooterApp.registerUser("Jane", "test123", 34);
+        scooterApp.registerUser("Kate", "4321", 23);
+        scooterApp.loginUser("Jane", "test123");
+        scooterApp.loginUser("Kate", "4321");
+        scooterApp.logoutUser("Jane");
+        scooterApp.logoutUser("Kate");
+        expect(scooterApp.registeredUsers).toEqual({
+            Jane: { age: 34, loggedIn: false, username: "Jane" },
+            Kate: { age: 23, loggedIn: false, username: "Kate" },
+        });
+    });
+
+    // rent scooter
+
+    // dock scooter
 });
-
-// log in
-
-// log out
-
-// rent scooter
-
-// dock scooter
